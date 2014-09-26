513afbaf6e54acdfc42ed276c285154adbc5bba6
rdsubhas <rdsubhas@gmail.com>
2014-09-26 23:18:21 +0530

    Added Image Slider

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,10 @@
 $(".full img").on("click", function() {
   $(this).toggleClass("zoom");
 });
+
+if ($("#blog-slider").length > 0) {
+  new IdealImageSlider.Slider({
+    selector: '#blog-slider',
+    effect: 'fade'
+  }).start();
+}
